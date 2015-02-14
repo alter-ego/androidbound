b1246f8a3d1fc5941fa7946fdefcb0da256de09e
Sasa Sekulic <ja@sasasekulic.info>
2015-02-15 00:52:20 +0100

    added click binding to TextView

--- src/com/alterego/androidbound/android/ui/BindableTextView.java
+++ src/com/alterego/androidbound/android/ui/BindableTextView.java
@@ -4,29 +4,36 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.TextView;
 
+import com.alterego.androidbound.interfaces.ICommand;
 import com.alterego.androidbound.interfaces.INotifyPropertyChanged;
 import com.alterego.androidbound.zzzztoremove.reactive.IObservable;
 import com.alterego.androidbound.zzzztoremove.reactive.ISubject;
 import com.alterego.androidbound.zzzztoremove.reactive.Subject;
 
-public class BindableTextView extends TextView implements INotifyPropertyChanged {
+public class BindableTextView extends TextView implements INotifyPropertyChanged, View.OnClickListener {
 
     private ISubject<String> propertyChanged;
 
     private boolean disposed;
 
+    private ICommand onClick = ICommand.empty;
+
     public BindableTextView(Context context) {
         super(context);
+        setOnClickListener(this);
     }
 
     public BindableTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setOnClickListener(this);
     }
 
     public BindableTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setOnClickListener(this);
     }
 
     public Typeface getTypeface() {
@@ -91,6 +98,24 @@ public class BindableTextView extends TextView implements INotifyPropertyChanged
 
     public int getBackgroundColor() {
         return 0;
+    }
+
+    public ICommand getClick() {
+        return onClick;
+    }
+
+    public void setClick(ICommand value) {
+        if(value == null) {
+            onClick = ICommand.empty;
+            return;
+        }
+        onClick = value;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(onClick.canExecute(null))
+            onClick.execute(null);
     }
 
 }
