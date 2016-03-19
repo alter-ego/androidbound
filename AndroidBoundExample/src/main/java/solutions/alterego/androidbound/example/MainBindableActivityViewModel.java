8ed156f02f351aa2d9d1281d1b8d2e5f7f19c1a0
Sasa Sekulic <ja@sasasekulic.info>
2016-03-19 23:20:47 +0100

    added MainBindingActivity, TextView, Button to example with color and font

--- AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/MainBindableActivityViewModel.java
+++ AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/MainBindableActivityViewModel.java
@@ -4,7 +4,6 @@ import com.alterego.advancedandroidlogger.interfaces.IAndroidLogger;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 
 import lombok.Getter;
 import lombok.experimental.Accessors;
@@ -12,20 +11,20 @@ import solutions.alterego.androidbound.ViewModel;
 import solutions.alterego.androidbound.interfaces.IDisposable;
 
 @Accessors(prefix = "m")
-public class MainActivityViewModel extends ViewModel implements IDisposable {
+public class MainBindableActivityViewModel extends ViewModel implements IDisposable {
 
     @Getter
     private String mMainActivityTitle;
 
     @Getter
-    private String mOpenBindableActivityText;
+    private String mOpenNormalActivityText;
 
-    public MainActivityViewModel(Activity activity, IAndroidLogger logger) {
+    public MainBindableActivityViewModel(Activity activity, IAndroidLogger logger) {
         setLogger(logger);
         setParentActivity(activity);
 
-        setMainActivityTitle("Main Activity");
-        setOpenBindableActivityText("Open Bindable Activity");
+        setMainActivityTitle("Bindable Activity");
+        setOpenNormalActivityText("Open Normal Activity");
     }
 
     public void setMainActivityTitle(String title) {
@@ -33,9 +32,9 @@ public class MainActivityViewModel extends ViewModel implements IDisposable {
         raisePropertyChanged("MainActivityTitle");
     }
 
-    public void setOpenBindableActivityText(String title) {
-        mOpenBindableActivityText = title;
-        raisePropertyChanged("OpenBindableActivityText");
+    public void setOpenNormalActivityText(String title) {
+        mOpenNormalActivityText = title;
+        raisePropertyChanged("OpenNormalActivityText");
     }
 
     @Override
@@ -43,23 +42,14 @@ public class MainActivityViewModel extends ViewModel implements IDisposable {
         //do nothing
     }
 
-    public boolean canOpenBindableActivity() {
+    public boolean canOpenNormalActivity() {
         return true;
     }
 
-    public void doOpenBindableActivity() {
-        Intent activityIntent = new Intent(getParentActivity(), MainBindingActivity.class);
+    public void doOpenNormalActivity() {
+        Intent activityIntent = new Intent(getParentActivity(), MainActivity.class);
         if (getParentActivity() != null) {
             getParentActivity().startActivity(activityIntent);
         }
     }
-
-    public int getMainActivityTitleColor() {
-        return Color.rgb(0, 255, 0);
-    }
-
-    public int getMainActivityBackgroundColor() {
-        return Color.rgb(200, 250, 250);
-    }
-
 }
--- AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/MainBindingActivity.java
+++ AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/MainBindingActivity.java
@@ -5,33 +5,44 @@ import com.alterego.advancedandroidlogger.interfaces.IAndroidLogger;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import solutions.alterego.androidbound.ViewBinder;
-import solutions.alterego.androidbound.ViewModel;
+import solutions.alterego.androidbound.android.BindingAppCompatActivity;
+import solutions.alterego.androidbound.interfaces.IViewBinder;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainBindingActivity extends BindingAppCompatActivity {
 
     public static final String LOGGING_TAG = "TEST_APP";
 
     private static final IAndroidLogger.LoggingLevel LOGGING_LEVEL = IAndroidLogger.LoggingLevel.VERBOSE;
 
+    private IViewBinder mViewBinder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         IAndroidLogger logger = new DetailedAndroidLogger(LOGGING_TAG, LOGGING_LEVEL);
+
         ViewBinder viewBinder = new ViewBinder(this, logger, null);
         viewBinder.getFontManager().setDefaultFont(Typeface.createFromAsset(getAssets(), "Roboto-Regular.ttf"));
         viewBinder.getFontManager().registerFont("light", Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf"));
         viewBinder.getFontManager().registerFont("italic", Typeface.createFromAsset(getAssets(), "Roboto-Italic.ttf"));
         viewBinder.getFontManager().registerFont("bold", Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf"));
 
-        ViewModel viewModel = new MainActivityViewModel(this, logger);
+        setViewBinder(viewBinder);
+        setBoundData(new MainBindableActivityViewModel(this, logger));
 
-        View view = viewBinder.inflate(this, viewModel, R.layout.activity_main, null);
-        setContentView(view);
+        setContentView(R.layout.activity_bindable_main);
     }
 
+    @Override
+    public IViewBinder getViewBinder() {
+        return mViewBinder;
+    }
+
+    @Override
+    public void setViewBinder(IViewBinder viewBinder) {
+        mViewBinder = viewBinder;
+    }
 }
