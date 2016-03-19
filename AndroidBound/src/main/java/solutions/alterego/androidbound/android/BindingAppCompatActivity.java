5cb53c815951b47eeadb477b2ab9688b79eeefed
Sasa Sekulic <ja@sasasekulic.info>
2016-03-19 23:20:06 +0100

    added BindingAppCompatActivity

---
+++ AndroidBound/src/main/java/solutions/alterego/androidbound/android/BindingAppCompatActivity.java
@@ -0,0 +1,60 @@
+package solutions.alterego.androidbound.android;
+
+import com.alterego.advancedandroidlogger.implementations.NullAndroidLogger;
+import com.alterego.advancedandroidlogger.interfaces.IAndroidLogger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import solutions.alterego.androidbound.android.interfaces.IBindableView;
+
+@Accessors(prefix = "m")
+public abstract class BindingAppCompatActivity extends AppCompatActivity implements IBindableView {
+
+    @Setter
+    protected IAndroidLogger mLogger = NullAndroidLogger.instance;
+
+    @Getter
+    @Setter
+    private Object mBoundData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getViewBinder() != null) {
+            getViewBinder().clearAllBindings();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (getViewBinder() != null) {
+            getViewBinder().clearAllBindings();
+        }
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        if (mBoundData == null) {
+            throw new RuntimeException("call setBoundData(Object) before calling setContentView!");
+        }
+
+        if (getViewBinder() == null) {
+            throw new RuntimeException("getViewBinder must not be null!");
+        }
+
+        View view = getViewBinder().inflate(this, mBoundData, layoutResID, null);
+        setContentView(view);
+    }
+
+    public void dispose() {
+        if (getViewBinder() != null) {
+            getViewBinder().clearAllBindings();
+        }
+    }
+}
