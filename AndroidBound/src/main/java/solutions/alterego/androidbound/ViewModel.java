1c1c8bd171794535975cba95ec58b4d93ad5e8e0
Sasa Sekulic <ja@sasasekulic.info>
2016-03-19 23:19:24 +0100

    added set/getParentActivity() to ViewModel

--- AndroidBound/src/main/java/solutions/alterego/androidbound/ViewModel.java
+++ AndroidBound/src/main/java/solutions/alterego/androidbound/ViewModel.java
@@ -3,17 +3,25 @@ package solutions.alterego.androidbound;
 import com.alterego.advancedandroidlogger.implementations.NullAndroidLogger;
 import com.alterego.advancedandroidlogger.interfaces.IAndroidLogger;
 
+import android.app.Activity;
+
+import java.lang.ref.WeakReference;
+
+import lombok.experimental.Accessors;
 import rx.Observable;
 import rx.subjects.PublishSubject;
-import solutions.alterego.androidbound.interfaces.INeedsLogger;
 import solutions.alterego.androidbound.binding.interfaces.INotifyPropertyChanged;
 import solutions.alterego.androidbound.interfaces.IDisposable;
+import solutions.alterego.androidbound.interfaces.INeedsLogger;
 
+@Accessors(prefix = "m")
 public class ViewModel implements INeedsLogger, INotifyPropertyChanged, IDisposable {
 
     protected transient IAndroidLogger mLogger = NullAndroidLogger.instance;
 
     private transient PublishSubject<String> propertyChanges = PublishSubject.create();
+
+    protected transient WeakReference<Activity> mParentActivity;
 
     protected void raisePropertyChanged(String property) {
         try {
@@ -35,4 +43,18 @@ public class ViewModel implements INeedsLogger, INotifyPropertyChanged, IDisposa
     public void setLogger(IAndroidLogger logger) {
         mLogger = logger.getLogger(this);
     }
+
+    public void setParentActivity(Activity activity) {
+        mParentActivity = new WeakReference<Activity>(activity);
+    }
+
+    public Activity getParentActivity() {
+        if (mParentActivity != null && mParentActivity.get() != null) {
+            return mParentActivity.get();
+        }
+
+        return null;
+    }
+
+
 }
