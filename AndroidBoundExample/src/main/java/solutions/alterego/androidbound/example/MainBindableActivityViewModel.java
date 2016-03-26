f55dde2046681baf46f0886675137ee00e172aa0
Sasa Sekulic <ja@sasasekulic.info>
2016-03-26 01:56:53 +0100

    changed bindable activity example image url to google logo

--- AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/MainBindableActivityViewModel.java
+++ AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/MainBindableActivityViewModel.java
@@ -100,6 +100,6 @@ public class MainBindableActivityViewModel extends ViewModel implements IDisposa
     }
 
     public String getImageViewSourceUrl() {
-        return "http://www.24sata.rs/resources/images/0000/056/479/cetinjska10_RAS_foto%20ana%20paunkovic_1000x0.jpg";
+        return "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
     }
 }
