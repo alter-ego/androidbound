29a77414b1dc3635b9e3b1be1859dd0dc77cfdcf
Sasa Sekulic <ja@sasasekulic.info>
2016-04-24 13:17:51 +0200

    changed listview example

--- AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/ListViewActivityViewModel.java
+++ AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/ListViewActivityViewModel.java
@@ -18,6 +18,8 @@ import solutions.alterego.androidbound.interfaces.IDisposable;
 @Accessors(prefix = "m")
 public class ListViewActivityViewModel extends ViewModel implements IDisposable {
 
+    private static final int listSize = 250;
+
     @Getter
     private String mListViewActivityTitle;
 
@@ -25,13 +27,18 @@ public class ListViewActivityViewModel extends ViewModel implements IDisposable 
     private String mOpenMainActivityText;
 
     @Getter
-    private List<ListViewItem> mExampleList;
+    private List<ListViewItem> mExampleList = new ArrayList<ListViewItem>();
 
     public ListViewActivityViewModel(Activity activity, IAndroidLogger logger) {
         setLogger(logger);
         setParentActivity(activity);
 
         setListViewActivityTitle("ListView activity");
+
+        for (int i = 0; i < listSize; i++) {
+            mExampleList.add(new ListViewItem(Integer.toString(i)));
+        }
+        raisePropertyChanged("ExampleList");
     }
 
     public void setListViewActivityTitle(String title) {
@@ -76,36 +83,20 @@ public class ListViewActivityViewModel extends ViewModel implements IDisposable 
         }
     }
 
-    public boolean canSelectListItem() {
+    public boolean canSelectListItem(ListViewItem item) {
         return true;
     }
 
-    public void doSelectListItem() {
-        Toast.makeText(getParentActivity(), "clicked list item!", Toast.LENGTH_SHORT).show();
-    }
-
-    public boolean canAddListItem() {
-        if (mExampleList == null || mExampleList.size() < 3) {
-            Toast.makeText(getParentActivity(), "added list item!", Toast.LENGTH_SHORT).show();
-            return true;
-        } else {
-            Toast.makeText(getParentActivity(), "not adding any more!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-    }
-
-    public void doAddListItem() {
-        if (mExampleList == null) {
-            mExampleList = new ArrayList<ListViewItem>();
-        }
-
-        mExampleList.add(new ListViewItem(Integer.toString(mExampleList.size())));
-        raisePropertyChanged("ExampleList");
+    public void doSelectListItem(ListViewItem item) {
+        Toast.makeText(getParentActivity(), "clicked list item = " + item.getTitle(), Toast.LENGTH_SHORT).show();
     }
 
     public static class ListViewItem {
         @Getter
         private String mTitle;
+
+        @Getter
+        private String mImageUrl = "https://www.google.co.uk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
 
         public ListViewItem(String title) {
             mTitle = title;
