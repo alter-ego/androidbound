e7ac4dabb319613e9c7d7aaa3025ed50c89f1470
emanuele <emanuele.g@gmail.com>
2016-11-10 15:47:46 +0100

    Added OnItemClickListener support to BindableRecyclerView

--- AndroidBound/src/main/java/solutions/alterego/androidbound/android/ui/BindableRecyclerView.java
+++ AndroidBound/src/main/java/solutions/alterego/androidbound/android/ui/BindableRecyclerView.java
@@ -1,12 +1,19 @@
 package solutions.alterego.androidbound.android.ui;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 
 import lombok.Getter;
@@ -15,10 +22,11 @@ import lombok.experimental.Accessors;
 import solutions.alterego.androidbound.android.adapters.BindableRecyclerViewAdapter;
 import solutions.alterego.androidbound.android.interfaces.IBindableView;
 import solutions.alterego.androidbound.android.ui.resources.BindingResources;
+import solutions.alterego.androidbound.interfaces.ICommand;
 import solutions.alterego.androidbound.interfaces.IViewBinder;
 
 @Accessors(prefix = "m")
-public class BindableRecyclerView extends RecyclerView implements IBindableView {
+public class BindableRecyclerView extends RecyclerView implements IBindableView, RecyclerView.OnItemTouchListener {
 
     private final int mItemTemplate;
 
@@ -31,6 +39,10 @@ public class BindableRecyclerView extends RecyclerView implements IBindableView 
 
     @Getter
     private BindableRecyclerViewAdapter mAdapter;
+
+    private ICommand mOnItemClickListener = ICommand.empty;
+
+    private final Rect mRect = new Rect();
 
     public BindableRecyclerView(Context context) {
         this(context, null);
@@ -45,6 +57,8 @@ public class BindableRecyclerView extends RecyclerView implements IBindableView 
 
         mItemTemplate = getItemTemplate(context, attrs);
         mTemplatesForObjects = new HashMap<>();
+
+        addOnItemTouchListener(this);
     }
 
     private int getItemTemplate(Context context, AttributeSet attrs) {
@@ -92,4 +106,104 @@ public class BindableRecyclerView extends RecyclerView implements IBindableView 
         }
     }
 
+    public ICommand getOnItemClickListener() {
+        return mOnItemClickListener;
+    }
+
+    public void setOnItemClickListener(ICommand l) {
+        if (l == null) {
+            mOnItemClickListener = ICommand.empty;
+            return;
+        }
+        mOnItemClickListener = l;
+    }
+
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        removeOnItemTouchListener(this);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        if (e.getAction() == MotionEvent.ACTION_UP) {
+            handleClick(e);
+        }
+        return false;
+    }
+
+    private View getChildOfAt(View view, int x, int y) {
+        if (!(view instanceof ViewGroup)) {
+            return view;
+        }
+        List<ViewGroup> viewGroups = new ArrayList<>();
+        viewGroups.add((ViewGroup) view);
+        ListIterator<ViewGroup> iterator = viewGroups.listIterator();
+
+        while (iterator.hasNext() || iterator.hasPrevious()) {
+            ViewGroup current;
+            if (iterator.hasNext()) {
+                current = iterator.next();
+            } else {
+                current = iterator.previous();
+            }
+
+            iterator.remove();
+            if (current == null) {
+                continue;
+            }
+
+            for (int i = 0; i < current.getChildCount(); i++) {
+                View childAt = current.getChildAt(i);
+                if (childAt instanceof ViewGroup) {
+                    iterator.add((ViewGroup) childAt);
+                    continue;
+                }
+                childAt.getHitRect(mRect);
+                mRect.set((int) (mRect.left + ViewCompat.getX(view)), (int) (mRect.top + ViewCompat.getY(view)),
+                        (int) (mRect.right + ViewCompat.getX(view)), (int) (mRect.bottom + ViewCompat.getY(view)));
+                if (mRect.contains(x, y)) {
+                    return childAt;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private void handleClick(MotionEvent event) {
+        View childView = findChildViewUnder(event.getX(), event.getY());
+        if (childView == null) {
+            return;
+        }
+
+        View childAt = getChildOfAt(childView, (int) event.getX(), (int) event.getY());
+        if (childAt != null && ViewCompat.hasOnClickListeners(childAt)) {
+            childAt.setPressed(true);
+            return;
+        }
+
+        final int position = getChildAdapterPosition(childView);
+        if (position == RecyclerView.NO_POSITION) {
+            return;
+        }
+        if (getItemsSource() == null) {
+            // Adapter is not BindableRecyclerViewAdaper or setItemSource hasn't been called
+            return;
+        }
+
+        if (mOnItemClickListener.canExecute(getItemsSource().get(position))) {
+            mOnItemClickListener.execute(getItemsSource().get(position));
+        }
+    }
+
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+    }
 }
