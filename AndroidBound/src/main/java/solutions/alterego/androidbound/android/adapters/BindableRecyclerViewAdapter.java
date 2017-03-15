8cd3c283c1c6ead9ee5e2855e2bded09ebaf7096
Emanuele Gringeri <emanuele.gringeri@fanmiles.com>
2017-03-15 13:07:34 +0100

    Fix: BindableRecyclerViewItemView#unbind never called. Remove superfluous handler

--- AndroidBound/src/main/java/solutions/alterego/androidbound/android/adapters/BindableRecyclerViewAdapter.java
+++ AndroidBound/src/main/java/solutions/alterego/androidbound/android/adapters/BindableRecyclerViewAdapter.java
@@ -1,6 +1,5 @@
 package solutions.alterego.androidbound.android.adapters;
 
-import android.os.Handler;
 import android.support.v4.util.Pair;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
@@ -47,8 +46,6 @@ public class BindableRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
     @Getter
     @Setter
     private RecyclerView.LayoutManager mLayoutManager;
-
-    private Handler mHandler = new Handler();
 
     private Subscription mSetValuesSubscription = Subscriptions.unsubscribed();
 
@@ -169,7 +166,9 @@ public class BindableRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
     & layout pass where you cannot change the RecyclerView data. Any method call that might change the structure
     of the RecyclerView or the adapter contents should be postponed to the next frame.*/
     private void postNotifyItemRangeRemoved(final int start, final int itemCount) {
-        mHandler.post(() -> notifyItemRangeRemoved(start, itemCount));
+        AndroidSchedulers.mainThread().createWorker().schedule(() -> {
+            notifyItemRangeRemoved(start, itemCount);
+        });
     }
 
 
@@ -217,6 +216,15 @@ public class BindableRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerVi
                     notifyDataSetChanged();
                 });
     }
+
+    @Override
+    public void onViewDetachedFromWindow(RecyclerView.ViewHolder holder) {
+        super.onViewDetachedFromWindow(holder);
+        if (holder instanceof BindableRecyclerViewItemViewHolder) {
+            ((BindableRecyclerViewItemViewHolder) holder).unbind();
+        }
+    }
+
 
     @Override
     public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
