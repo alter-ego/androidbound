221e4f808a35c07ae43771bc4270fb25ded97117
Sasa Sekulic <ja@sasasekulic.info>
2017-05-03 01:07:23 +0200

    added delegate to ImageView, now extending AppCompatImageView (fixes #16)

--- AndroidBound/src/main/java/solutions/alterego/androidbound/android/ui/BindableImageView.java
+++ AndroidBound/src/main/java/solutions/alterego/androidbound/android/ui/BindableImageView.java
@@ -3,46 +3,115 @@ package solutions.alterego.androidbound.android.ui;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v7.widget.AppCompatImageView;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import rx.Observable;
-import rx.subjects.PublishSubject;
 import solutions.alterego.androidbound.android.interfaces.IImageLoader;
 import solutions.alterego.androidbound.android.interfaces.INeedsImageLoader;
 import solutions.alterego.androidbound.binding.interfaces.INotifyPropertyChanged;
 import solutions.alterego.androidbound.interfaces.ICommand;
 
-public class BindableImageView extends ImageView implements OnClickListener, INotifyPropertyChanged, OnLongClickListener, INeedsImageLoader {
+public class BindableImageView extends AppCompatImageView implements INotifyPropertyChanged, INeedsImageLoader {
 
-
-    private boolean disposed;
+    private BindableViewDelegate mDelegate;
 
     private int currentResId;
-
-    private PublishSubject<String> propertyChanged = PublishSubject.create();
-
-    private ICommand onLongClick = ICommand.empty;
-
-    private ICommand onClick = ICommand.empty;
 
     private String source;
 
     private IImageLoader mImageLoader = IImageLoader.nullImageLoader;
 
+    public BindableImageView(Context context) {
+        this(context, null);
+    }
+
     public BindableImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mDelegate = createDelegate(this);
     }
 
     public BindableImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mDelegate = createDelegate(this);
     }
+
+
+    /****** beginning of the delegated methods ******/
+
+    protected BindableViewDelegate createDelegate(View view) {
+        return new BindableViewDelegate(view);
+    }
+
+    public ICommand getClick() {
+        return mDelegate.getClick();
+    }
+
+    public void setClick(ICommand value) {
+        mDelegate.setClick(value);
+    }
+
+    public int getBackgroundColor() {
+        return mDelegate.getBackgroundColor();
+    }
+
+    public void setBackgroundColor(int color) {
+        mDelegate.setBackgroundColor(color);
+        super.setBackgroundColor(color);
+    }
+
+    public StateListDrawable getBackgroundDrawableState() {
+        return mDelegate.getBackgroundDrawableState();
+    }
+
+    public void setBackgroundDrawableState(StateListDrawable colors) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            super.setBackground(colors);
+            mDelegate.setBackgroundDrawableState(colors);
+        }
+    }
+
+    public int getBackgroundResource() {
+        return 0;
+    }
+
+    public void setBackgroundResource(int res) {
+        super.setBackgroundResource(res);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mDelegate.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    public void setWidth(int width) {
+        mDelegate.setWidth(width);
+    }
+
+    public void setHeight(int height) {
+        mDelegate.setHeight(height);
+    }
+
+    @Override
+    public Observable<String> onPropertyChanged() {
+        return mDelegate.onPropertyChanged();
+    }
+
+    @Override
+    public void dispose() {
+        mDelegate.dispose();
+        mImageLoader = IImageLoader.nullImageLoader;
+        source = null;
+        currentResId = 0;
+    }
+
+    /****** end of the delegated methods ******/
 
     public String getSource() {
         return source;
@@ -65,99 +134,6 @@ public class BindableImageView extends ImageView implements OnClickListener, INo
         setSource(value.toString());
     }
 
-    public ICommand getClick() {
-        return onClick;
-    }
-
-    public void setClick(ICommand value) {
-        if (value == null) {
-            setClickable(false);
-            setOnClickListener(null);
-            onClick = ICommand.empty;
-            return;
-        }
-        setClickable(true);
-        setOnClickListener(this);
-        onClick = value;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (onClick.canExecute(null)) {
-            onClick.execute(null);
-        }
-    }
-
-    public ICommand getLongClick() {
-        return onLongClick;
-    }
-
-    public void setLongClick(ICommand value) {
-        if (value == null) {
-            setClickable(false);
-            setOnLongClickListener(null);
-            onLongClick = ICommand.empty;
-            return;
-        }
-        setClickable(true);
-        setOnLongClickListener(this);
-        onLongClick = value;
-    }
-
-    @Override
-    public boolean onLongClick(View arg0) {
-
-        if (onLongClick.canExecute(null)) {
-            onLongClick.execute(null);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public void dispose() {
-        if (disposed) {
-            return;
-        }
-
-        disposed = true;
-        if (propertyChanged != null) {
-            propertyChanged.onCompleted();
-            propertyChanged = null;
-        }
-
-        propertyChanged = null;
-        onClick = null;
-        onLongClick = null;
-    }
-
-    @Override
-    public Observable<String> onPropertyChanged() {
-        if (propertyChanged == null) {
-            propertyChanged = PublishSubject.create();
-        }
-
-        return propertyChanged;
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-
-        if (disposed || propertyChanged == null) {
-            return;
-        }
-
-        if (w != oldw) {
-            propertyChanged.onNext("Width");
-        }
-
-        if (h != oldh) {
-            propertyChanged.onNext("Height");
-        }
-    }
-
     public void setBitmap(Bitmap bmp) {
         setImageBitmap(bmp);
     }
@@ -173,26 +149,6 @@ public class BindableImageView extends ImageView implements OnClickListener, INo
     public void setResource(Integer resId) {
         currentResId = resId;
         setImageResource(currentResId);
-    }
-
-    public void setWidth(int width) {
-        if (width == getWidth()) {
-            return;
-        }
-
-        ViewGroup.LayoutParams p = getLayoutParams();
-        p.width = width;
-        setLayoutParams(p);
-    }
-
-    public void setHeight(int height) {
-        if (height == getHeight()) {
-            return;
-        }
-
-        ViewGroup.LayoutParams p = getLayoutParams();
-        p.height = height;
-        setLayoutParams(p);
     }
 
     @Override
