231fc09dc375283d035abe493dd1ef61a44bb17d
Sasa Sekulic <ja@sasasekulic.info>
2017-05-03 19:23:38 +0200

    added delegate to Linear & Relative layout, Horizontal SV

--- AndroidBound/src/main/java/solutions/alterego/androidbound/android/ui/BindableRelativeLayout.java
+++ AndroidBound/src/main/java/solutions/alterego/androidbound/android/ui/BindableRelativeLayout.java
@@ -1,138 +1,66 @@
 package solutions.alterego.androidbound.android.ui;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import rx.Observable;
-import rx.subjects.PublishSubject;
-import solutions.alterego.androidbound.interfaces.ICommand;
 import solutions.alterego.androidbound.binding.interfaces.INotifyPropertyChanged;
+import solutions.alterego.androidbound.interfaces.ICommand;
 
-public class BindableRelativeLayout extends RelativeLayout implements INotifyPropertyChanged, OnClickListener {
+public class BindableRelativeLayout extends RelativeLayout implements INotifyPropertyChanged {
 
-    private boolean disposed;
-
-    private PublishSubject<String> propertyChanged = PublishSubject.create();
-
-    private ICommand onClick = ICommand.empty;
+    private BindableViewDelegate mDelegate;
 
     public BindableRelativeLayout(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public BindableRelativeLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mDelegate = createDelegate(this);
     }
 
     public BindableRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mDelegate = createDelegate(this);
     }
 
-    @Override
-    public Observable<String> onPropertyChanged() {
-        if (propertyChanged == null) {
-            propertyChanged = PublishSubject.create();
-        }
+    /****** beginning of the delegated methods, to be copy/pasted in every bindable view ******/
 
-        return propertyChanged;
-    }
-
-    @Override
-    public void dispose() {
-        if (disposed) {
-            return;
-        }
-
-        disposed = true;
-        if (propertyChanged != null) {
-            propertyChanged.onCompleted();
-            propertyChanged = null;
-        }
-
-        propertyChanged = null;
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-
-        if (disposed || propertyChanged == null) {
-            return;
-        }
-
-        if (w != oldw) {
-            propertyChanged.onNext("Width");
-        }
-
-        if (h != oldh) {
-            propertyChanged.onNext("Height");
-        }
-    }
-
-    public void setWidth(int width) {
-        if (width == getWidth()) {
-            return;
-        }
-
-        ViewGroup.LayoutParams p = getLayoutParams();
-        p.width = width;
-        setLayoutParams(p);
-    }
-
-    public void setHeight(int height) {
-        if (height == getHeight()) {
-            return;
-        }
-
-        ViewGroup.LayoutParams p = getLayoutParams();
-        p.height = height;
-        setLayoutParams(p);
+    protected BindableViewDelegate createDelegate(View view) {
+        return new BindableViewDelegate(view);
     }
 
     public ICommand getClick() {
-        return onClick;
+        return mDelegate.getClick();
     }
 
     public void setClick(ICommand value) {
-        if (value == null) {
-            setClickable(false);
-            setOnClickListener(null);
-            onClick = ICommand.empty;
-            return;
-        }
-        setClickable(true);
-        setOnClickListener(this);
-        onClick = value;
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (onClick.canExecute(null)) {
-            onClick.execute(null);
-        }
+        mDelegate.setClick(value);
     }
 
     public int getBackgroundColor() {
-        return 0;
+        return mDelegate.getBackgroundColor();
     }
 
     public void setBackgroundColor(int color) {
+        mDelegate.setBackgroundColor(color);
         super.setBackgroundColor(color);
     }
 
-    @SuppressLint("NewApi")
-    public void setBackground(Drawable bgd) {
-        try {
-            super.setBackground(bgd);
-        } catch (Exception e) {
-            //failed because of API level, probably
-            super.setBackgroundDrawable(bgd);
+    public StateListDrawable getBackgroundDrawableState() {
+        return mDelegate.getBackgroundDrawableState();
+    }
+
+    public void setBackgroundDrawableState(StateListDrawable colors) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            super.setBackground(colors);
+            mDelegate.setBackgroundDrawableState(colors);
         }
     }
 
@@ -148,9 +76,33 @@ public class BindableRelativeLayout extends RelativeLayout implements INotifyPro
         return 0;
     }
 
-    @SuppressLint("NewApi")
-    @SuppressWarnings("deprecation")
     public void setBackgroundDrawable(Drawable res) {
         super.setBackgroundDrawable(res);
     }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mDelegate.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    public void setWidth(int width) {
+        mDelegate.setWidth(width);
+    }
+
+    public void setHeight(int height) {
+        mDelegate.setHeight(height);
+    }
+
+    @Override
+    public Observable<String> onPropertyChanged() {
+        return mDelegate.onPropertyChanged();
+    }
+
+    @Override
+    public void dispose() {
+        mDelegate.dispose();
+    }
+
+    /****** end of the delegated methods, to be copy/pasted in every bindable view ******/
 }
