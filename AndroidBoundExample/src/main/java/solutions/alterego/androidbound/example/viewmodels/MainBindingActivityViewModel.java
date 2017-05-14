cb9c7674ab9bf403a4d81188b89d05cf35d33683
Sasa Sekulic <ja@sasasekulic.info>
2017-05-14 17:28:50 +0200

    moved view binding examples from main activity to main bindable activity

--- AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/viewmodels/MainBindingActivityViewModel.java
+++ AndroidBoundExample/src/main/java/solutions/alterego/androidbound/example/viewmodels/MainBindingActivityViewModel.java
@@ -4,12 +4,16 @@ import org.joda.time.DateTime;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.widget.Toast;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 import solutions.alterego.androidbound.ViewModel;
 import solutions.alterego.androidbound.example.MainActivity;
+import solutions.alterego.androidbound.example.R;
 import solutions.alterego.androidbound.interfaces.ILogger;
 
 @Accessors(prefix = "m")
@@ -24,6 +28,16 @@ public class MainBindingActivityViewModel extends ViewModel {
     @Getter
     private boolean mTextViewVisible = false;
 
+    @Getter
+    private String mEditTextText = "empty edit text";
+
+    @Getter
+    private String mTextViewBoundToEditText = "empty";
+
+    @Getter
+    @Setter
+    private String mBoundEditTextText = mEditTextText;
+
     public MainBindingActivityViewModel(Activity activity, ILogger logger) {
         setLogger(logger);
         setParentActivity(activity);
@@ -32,9 +46,19 @@ public class MainBindingActivityViewModel extends ViewModel {
         setOpenNormalActivityText("Open Normal Activity");
     }
 
+    @Override
+    public void onCreate(Bundle outState) {
+        //do nothing
+        raisePropertyChanged("BoundEditTextText");
+    }
+
     public void setMainActivityTitle(String title) {
         mMainActivityTitle = title;
         raisePropertyChanged("MainActivityTitle");
+    }
+
+    public int getMainActivityTitleColor() {
+        return Color.rgb(0, 255, 0);
     }
 
     public void setOpenNormalActivityText(String title) {
@@ -78,7 +102,62 @@ public class MainBindingActivityViewModel extends ViewModel {
         return "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
     }
 
+    public int getImageViewResource() {
+        return R.mipmap.ic_launcher;
+    }
+
+    public boolean canImageViewClick() {
+        return true;
+    }
+
+    public void doImageViewClick() {
+        Toast.makeText(getParentActivity(), "clicked ImageView!", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean canImageViewLongClick() {
+        return true;
+    }
+
+    public void doImageViewLongClick() {
+        Toast.makeText(getParentActivity(), "long clicked ImageView!", Toast.LENGTH_SHORT).show();
+    }
+
     public DateTime getCurrentDate() {
         return DateTime.now();
+    }
+
+    public boolean canClearEditTextText() {
+        return true;
+    }
+
+    public void doClearEditTextText() {
+        setBoundEditTextText("");
+        raisePropertyChanged("BoundEditTextText");
+    }
+
+    public void setEditTextText(String text) {
+        mLogger.info("text = " + text);
+        mTextViewBoundToEditText = text;
+        raisePropertyChanged("TextViewBoundToEditText");
+    }
+
+    public int getEditTextColor() {
+        return Color.rgb(255, 0, 255);
+    }
+
+    public boolean canTextViewClick() {
+        return true;
+    }
+
+    public void doTextViewClick() {
+        Toast.makeText(getParentActivity(), "clicked text view!", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean canTextViewLongClick() {
+        return true;
+    }
+
+    public void doTextViewLongClick() {
+        Toast.makeText(getParentActivity(), "long clicked text view!", Toast.LENGTH_SHORT).show();
     }
 }
