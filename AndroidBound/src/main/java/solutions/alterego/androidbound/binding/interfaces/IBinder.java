9ac315bd0cd15e968276c267fd8706c3cbcca8cc
Sasa Sekulic <ja@sasasekulic.info>
2017-07-19 18:46:51 +0200

    IBinder.bind now specifically uses View for target

--- AndroidBound/src/main/java/solutions/alterego/androidbound/binding/interfaces/IBinder.java
+++ AndroidBound/src/main/java/solutions/alterego/androidbound/binding/interfaces/IBinder.java
@@ -1,12 +1,12 @@
 package solutions.alterego.androidbound.binding.interfaces;
 
+import android.view.View;
+
 import java.util.List;
 
-import solutions.alterego.androidbound.binding.interfaces.IBindingAssociationEngine;
 import solutions.alterego.androidbound.interfaces.INeedsLogger;
-
 
 public interface IBinder extends INeedsLogger {
 
-    List<IBindingAssociationEngine> bind(Object source, Object target, String bindingSpecifications);
+    List<IBindingAssociationEngine> bind(Object source, View target, String bindingSpecifications);
 }
