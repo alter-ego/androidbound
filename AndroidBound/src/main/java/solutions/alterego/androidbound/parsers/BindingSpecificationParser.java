e78cb602f76510b70808c5d7ab05231360f42b58
Sasa Sekulic <ja@sasasekulic.info>
2017-07-20 18:21:09 +0200

    extracted string escaping from BindingSpecificationParser

--- AndroidBound/src/main/java/solutions/alterego/androidbound/parsers/BindingSpecificationParser.java
+++ AndroidBound/src/main/java/solutions/alterego/androidbound/parsers/BindingSpecificationParser.java
@@ -10,6 +10,7 @@ import solutions.alterego.androidbound.helpers.Matcher;
 import solutions.alterego.androidbound.helpers.Pattern;
 import solutions.alterego.androidbound.interfaces.ILogger;
 import solutions.alterego.androidbound.resources.interfaces.IResourceProvider;
+import solutions.alterego.androidbound.utils.StringUtils;
 
 public class BindingSpecificationParser implements IParser<BindingSpecification> {
 
@@ -35,60 +36,6 @@ public class BindingSpecificationParser implements IParser<BindingSpecification>
         setLogger(logger);
     }
 
-    final static String unescape(String value) {
-        if (value == null) {
-            return null;
-        }
-        StringBuffer result = new StringBuffer(value.length());
-        boolean appendBackSlash = false;
-
-        for (int i = 0; i < value.length(); i++) {
-            int code = value.codePointAt(i);
-            if (value.codePointAt(i) > Character.MAX_VALUE) {
-                i++;
-            }
-
-            if (!appendBackSlash) {
-                if (code == '\\') {
-                    appendBackSlash = true;
-                } else {
-                    result.append(Character.toChars(code));
-                }
-                continue;
-            }
-
-            if (code == '\\') {
-                appendBackSlash = false;
-                result.append('\\');
-                result.append('\\');
-                continue;
-            }
-
-            switch (code) {
-                case 'r':
-                    result.append('\r');
-                    break;
-                case 'n':
-                    result.append('\n');
-                    break;
-                case 'f':
-                    result.append('\f');
-                    break;
-                case 't':
-                    result.append('\t');
-                    break;
-                default:
-                    result.append(Character.toChars(code));
-                    break;
-            }
-            appendBackSlash = false;
-        }
-        if (appendBackSlash) {
-            result.append('\\');
-        }
-        return result.toString();
-    }
-
     public void setLogger(ILogger logger) {
         mLogger = logger.getLogger(this);
     }
@@ -112,7 +59,7 @@ public class BindingSpecificationParser implements IParser<BindingSpecification>
         if (namedValue != null) {
             return mResourceProvider.find(namedValue);
         }
-        return unescape(constValue);
+        return StringUtils.unescape(constValue);
     }
 
     private BindingMode parseMode(String value) {
